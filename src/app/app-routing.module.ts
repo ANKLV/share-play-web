a1c3ac8a323230d4c8b08b2af3cd31b52fc15a7d
andrew-lukyanov <nefesh.samael@gmail.com>
2021-03-30 17:05:44 +0300

    add home page route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { PlaylistComponent } from './components/playlist/playlist.component';
 
 const routes: Routes = [
+  { path: '' , redirectTo: 'playlists', pathMatch: 'full' },
   { path: 'playlists', component: PlaylistComponent }
 ];
 
