b054a8a49e6a585fd676a234dbcf5d2fb210c03d
andrew-lukyanov <nefesh.samael@gmail.com>
2021-03-31 12:56:09 +0300

    Add version to action path

--- src/app/api/playlist.ts
+++ src/app/api/playlist.ts
@@ -10,6 +10,10 @@ export class PlaylistAPI {
   constructor(private http: HttpClient) {  }
 
   query(params = {}) {
-    return this.http.get(`${environment.apiEndpoint}/playlists`, { params: params });
+    return this.http.get(`${environment.apiEndpoint}/api/v1/playlists`, { params: params });
+  }
+
+  create(params:any) {
+    return this.http.post(`${environment.apiEndpoint}/api/v1/playlists`, params);
   }
 }
