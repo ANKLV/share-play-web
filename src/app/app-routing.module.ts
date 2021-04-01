c5e2d5665807c92d82ac7d061c4e9554f4b07d60
andrew-lukyanov <nefesh.samael@gmail.com>
2021-04-01 11:58:30 +0300

    button inline

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PlaylistsComponent } from './components/playlists/playlists.component';
-import { PlaylistFormComponent } from './components/playlist-form/playlist-form.component';
 
 const routes: Routes = [
   { path: '' , redirectTo: 'playlists', pathMatch: 'full' },
   { path: 'playlists', component: PlaylistsComponent },
-  { path: 'playlist-form', component: PlaylistFormComponent }
 ];
 
 @NgModule({
--- src/app/components/playlist-form/playlist-form.component.ts
+++ src/app/components/playlist-form/playlist-form.component.ts
@@ -1,31 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { PlaylistAPI } from '../../api';
 
 @Component({
   selector: 'app-playlist-form',
-  templateUrl: './playlist-form.component.html',
-  styleUrls: ['./playlist-form.component.scss']
+  templateUrl: './playlist-form.component.html'
 })
-export class PlaylistFormComponent implements OnInit {
+
+export class PlaylistFormComponent {
+  @Output() onCreate = new EventEmitter<any>();
   playlistForm = new FormGroup({
     title: new FormControl(null, Validators.required)
   })
 
   constructor(private playlistAPI: PlaylistAPI) { }
 
-  ngOnInit(): void {
-    this.createPlaylist();
-  }
-
   createPlaylist() {
-    console.log(this.playlistForm.value)
     this.playlistAPI.create({playlist: this.playlistForm.value}).subscribe((data) => {
-      console.log(data)
+      this.onCreate.emit(data);
+      this.playlistForm.reset();
     }, (error) => {
       console.log('error', error);
     })
-    this.playlistForm.reset();
   }
-
 }
