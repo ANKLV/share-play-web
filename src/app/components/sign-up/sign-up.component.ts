6ebfc08525e427438b1bb267d71bfde3dfeefbde
andrew-lukyanov <nefesh.samael@gmail.com>
2021-05-12 11:49:46 +0300

    Add password confirmation

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,5 +1,6 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { UserAPI } from '../../api';
 import { Toastr } from "../../providers"
 
@@ -13,22 +14,20 @@ export class SignUpComponent {
   userForm = new FormGroup({
     email: new FormControl(null, Validators.required),
     nickname: new FormControl(null, Validators.required),
-    password: new FormControl(null, Validators.required)
+    password: new FormControl(null, Validators.required),
+    password_confirmation: new FormControl(null, Validators.required)
   })
 
-  constructor(private userAPI: UserAPI, private toastr: Toastr) { }
+  constructor(private userAPI: UserAPI, private toastr: Toastr, private router: Router) { }
 
   createUser() {
     this.userAPI.create({user: this.userForm.value}).subscribe((data) => {
       this.onCreate.emit(data);
-      this.showSuccess();
+      this.toastr.success("Registered");
+      this.router.navigate(['/playlists']);
     }, (error) => {
       console.log('error', error);
       this.toastr.showResponseErrors(error)
     })
-  }
-
-  showSuccess() {
-    this.toastr.success("Registered");
   }
 }
--- src/app/providers/toastr.ts
+++ src/app/providers/toastr.ts
@@ -12,12 +12,12 @@ export class Toastr {
     this.toast.success(message);
   }
   error(message:string) {
-    this.toast.error(message, "null", {enableHtml: true});
+    this.toast.error(message, "Sorry", {enableHtml: true});
   }
   showResponseErrors = (resp:any) => {
     try {
       const errors = resp.error.error || resp.error.message || ['Something went wrong try again later'];
-      this.toast.error(errors, "null", {enableHtml: true});
+      this.toast.error(errors, "Sorry", {enableHtml: true});
     } catch (ex) {
       this.toast.error('Something went wrong try again later');
     };
